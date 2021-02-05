7fa55cafe73f41a5e4eb4d8e076f9bb344dc3282
a.kontopoulos <a.kontopoulos@gmail.com>
2021-02-05 21:58:54 +0200

    create file for storing grammar facts

---
+++ src/grammar.js
@@ -0,0 +1,14 @@
+export const grammar = {
+  article: {
+    type: 'article',
+    facts: ['lakes and mountains do not take articles', 'Cities and countries do not take articles', 'There are definate and indefinate articles']
+  },
+  noun: {
+    type: 'noun',
+    facts: ['irregular plural nouns', 'gerunds']
+  },
+  adverb: {
+    type: 'adverb',
+    facts: ['fast is an adjective and an adverb', 'frequency adverbs are: rarely, often, seldom' ]
+  }
+};
