242f2a65aae8d004436191c56271c547c0acd933
a.kontopoulos <a.kontopoulos@gmail.com>
2021-02-05 22:25:28 +0200

    add basic jsx to start app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,54 @@
+import { grammar } from './grammar';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = () => (
+
+const title = ""
+
+const facts = []
+
+for(const type in grammar) {
+  facts.push(<div
+    key = {type}
+    className = 'grammar'
+    alt = {type}
+    ariaLabel = {type}
+    role = "button"
+    onClick = {displayFact}>
+    {type}
+    </div>
+
+   )
+}
+
+function displayFact(e) {
+  const selectedType = e.target.alt;
+  const grammarInfo = grammar[selectedType];
+  const optionIndex = Math.floor(Math.random() * grammarInfo.facts.length);
+  const funFact = grammarInfo.facts[optionIndex];
+  document.getElementById("fact").innerHTML = funFact;
+}
+
+const background = (<img
+  className = "background"
+  alt = "ocean"
+  src = "/images/ocean.jpg"
+  />
+)
+
+const grammarFacts = (
   <div>
-    <p>Hello world</p>
+    <h1>{title === "" ? "Click a grammar type for a fact" : title }  </h1>
+    <p id = "fact"></p>
+    <div className = "">
+      {facts}
+    </div>
   </div>
+)
+
+const App = () => (
+  grammarFacts
 )
 
 ReactDOM.render(
